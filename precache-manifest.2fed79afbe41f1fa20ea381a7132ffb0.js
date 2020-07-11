f7ac45955bb3180e5bd075f13e4856afe0895454
Florian Motteau <florian@motteau-martins.net>
2020-07-11 15:29:59 -0400

    Updates

---
+++ precache-manifest.2fed79afbe41f1fa20ea381a7132ffb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2e504132b843e59eecba471647cfda",
+    "url": "/goat-keeper/index.html"
+  },
+  {
+    "revision": "4f6360c6c7099aaff171",
+    "url": "/goat-keeper/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "10971a9b0b0bea451611",
+    "url": "/goat-keeper/static/css/main.d3c2207c.chunk.css"
+  },
+  {
+    "revision": "4f6360c6c7099aaff171",
+    "url": "/goat-keeper/static/js/2.8c09030b.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/goat-keeper/static/js/2.8c09030b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10971a9b0b0bea451611",
+    "url": "/goat-keeper/static/js/main.8c8d7e11.chunk.js"
+  },
+  {
+    "revision": "f2a176d118bc94f1ae2f",
+    "url": "/goat-keeper/static/js/runtime-main.d49da317.js"
+  }
+]);
