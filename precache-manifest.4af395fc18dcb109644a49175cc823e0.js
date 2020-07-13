fe29ff8781d8653713f352028d3ef6f259390d30
Florian Motteau <florian@motteau-martins.net>
2020-07-13 13:25:35 -0400

    Updates

---
+++ precache-manifest.4af395fc18dcb109644a49175cc823e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4297ee7fe9ee1875c147a8e023b0aa4d",
+    "url": "/goat-keeper/index.html"
+  },
+  {
+    "revision": "756a5bfaeebe4579ac88",
+    "url": "/goat-keeper/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "ee3394ca4dd6ec8134ce",
+    "url": "/goat-keeper/static/css/main.50af2471.chunk.css"
+  },
+  {
+    "revision": "756a5bfaeebe4579ac88",
+    "url": "/goat-keeper/static/js/2.3c6911b2.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/goat-keeper/static/js/2.3c6911b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee3394ca4dd6ec8134ce",
+    "url": "/goat-keeper/static/js/main.88ace8d8.chunk.js"
+  },
+  {
+    "revision": "f2a176d118bc94f1ae2f",
+    "url": "/goat-keeper/static/js/runtime-main.d49da317.js"
+  },
+  {
+    "revision": "78b8094996a45882438ab289b0facde0",
+    "url": "/goat-keeper/static/media/volleyball.78b80949.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goat-keeper/precache-manifest.aba6b0c659a850c0e513b9cb749ba901.js"
+  "/goat-keeper/precache-manifest.4af395fc18dcb109644a49175cc823e0.js"
 );
 
 self.addEventListener('message', (event) => {
